e650ac8157ca25fb3a2c13406785ae114aad806e
En3_HCl <En3_HCl_K@icloud.com>
2020-12-08 23:09:07 +0900

    release information add

--- KanaKanjier/Setting/UpdateInformationView.swift
+++ KanaKanjier/Setting/UpdateInformationView.swift
@@ -10,36 +10,34 @@ import SwiftUI
 struct UpdateInfomationView: View {
     var body: some View {
         Form{
-            Section{
-                VStack(alignment: .leading){
-                    HeadlineView("1.2", releaseDate: "2020年12月xx日")
-                    ParagraphView("azooKeyユーザ辞書機能を追加しました。設定からご利用いただけます。", points: [])
-                    ParagraphView("キーの文字サイズを変更する機能を追加しました。設定からご利用いただけます。", points: [])
-                    ParagraphView("記号の変換を改善しました。", points: [])
-                    ParagraphView("その他軽微な操作性の改善を行いました。", points: [])
-                }
+            VersionView("1.2", releaseDate: "2020年12月09日"){
+                ParagraphView("機能を追加しました。", points: [
+                    "azooKeyユーザ辞書機能を追加",
+                    "キーの文字サイズを変更する機能を追加",
+                    "ローマ字での日本語入力中、英単語の変換候補も表示するよう変更",
+                ])
+                ParagraphView("不具合を修正しました。", points: [
+                    "ローマ字での英語入力中、一部の候補を選択した場合に正しく変換されない問題",
+                ])
+                ParagraphView("記号の変換を改善しました。", points: [])
+                ParagraphView("その他軽微なデザイン・操作性の改善を行いました。", points: [])
             }
 
-            Section{
-                VStack(alignment: .leading){
-                    HeadlineView("1.1", releaseDate: "2020年12月06日")
-                    ParagraphView("機能を改善しました。", points: [
-                        "ローマ字入力中の矢印入力に対応",
-                        "unicode変換の接頭辞を拡大",
-                        "数字の装飾文字変換に対応"
-                    ])
-                    ParagraphView("次の問題を修正しました。", points: [
-                        "一部候補が正しく学習されない問題",
-                        "ローマ字入力中の変換の不具合"
-                    ])
-                    ParagraphView("その他軽微なデザインの修正を行いました。", points: [])
-                }
+            VersionView("1.1", releaseDate: "2020年12月06日"){
+                ParagraphView("機能を改善しました。", points: [
+                    "ローマ字入力中の矢印入力に対応",
+                    "unicode変換の接頭辞を拡大",
+                    "数字の装飾文字変換に対応"
+                ])
+                ParagraphView("次の問題を修正しました。", points: [
+                    "一部候補が正しく学習されない問題",
+                    "ローマ字入力中の変換の不具合"
+                ])
+                ParagraphView("その他軽微なデザインの修正を行いました。", points: [])
             }
-            Section{
-                VStack(alignment: .leading){
-                    HeadlineView("1.0", releaseDate: "2020年12月04日")
-                    ParagraphView("azooKeyを公開しました。", points: [])
-                }
+
+            VersionView("1.0", releaseDate: "2020年12月04日"){
+                ParagraphView("azooKeyを公開しました。", points: [])
             }
         }.navigationBarTitle(Text("更新履歴"), displayMode: .inline)
     }
@@ -55,14 +53,14 @@ private struct HeadlineView: View {
     }
 
     var body: some View {
-        HStack{
+        HStack(alignment: .bottom){
             Text("ver \(version)")
-                .font(.headline)
+                .font(.title2)
             Spacer()
             Text("\(releaseDate)配信")
                 .font(.subheadline)
         }
-        .padding(.bottom)
+        .padding(2)
     }
 
 }
@@ -77,12 +75,37 @@ private struct ParagraphView: View {
 
     var body: some View {
         VStack(alignment: .leading){
-            Text("・\(headline)")
-            ForEach(points, id: \.self){point in
-                Text(" - \(point)")
+            Text("⚫︎\(headline)")
+            if !points.isEmpty{
+                Text(points.map{"・\($0)"}.joined(separator: "\n"))
             }
         }
-        .padding(.bottom, 1)
+        .multilineTextAlignment(.leading)
+        .padding(.bottom, 3)
+    }
+
+}
+
+private struct VersionView<Content: View>: View {
+    private let content: () -> Content
+
+    private let version: String
+    private let releaseDate: String
+
+    init(_ version: String, releaseDate: String, @ViewBuilder _ content: @escaping () -> Content){
+        self.content = content
+        self.version = version
+        self.releaseDate = releaseDate
+    }
+
+    var body: some View {
+        Section{
+            VStack(alignment: .leading){
+                HeadlineView(version, releaseDate: releaseDate)
+                content()
+            }
+            .multilineTextAlignment(.leading)
+        }
     }
 
 }
