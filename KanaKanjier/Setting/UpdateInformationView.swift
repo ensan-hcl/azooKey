d41b6b56591c27a45c4a52cc8ad40356429d4430
En3_HCl <En3_HCl_K@icloud.com>
2021-01-18 15:21:10 +0900

    update and dictionary update

--- KanaKanjier/Setting/UpdateInformationView.swift
+++ KanaKanjier/Setting/UpdateInformationView.swift
@@ -10,6 +10,10 @@ import SwiftUI
 struct UpdateInfomationView: View {
     var body: some View {
         Form{
+            VersionView("1.4.3", releaseDate: "2021年01月18日"){
+                ParagraphView("不統一なデザインを修正しました。")
+                ParagraphView("辞書を更新しました。")
+            }
             VersionView("1.4.2", releaseDate: "2021年01月08日"){
                 ParagraphView("不具合を修正しました。"){
                     "キーボードの種類の変更が反映されない事がある問題の修正"
