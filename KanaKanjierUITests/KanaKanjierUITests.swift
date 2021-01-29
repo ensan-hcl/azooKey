59d2252a71d75816cefb962bb766d62d5fc3443b
En3_HCl <En3_HCl_K@icloud.com>
2021-01-30 00:01:13 +0900

    fix deprecated metrics

--- KanaKanjierUITests/KanaKanjierUITests.swift
+++ KanaKanjierUITests/KanaKanjierUITests.swift
@@ -33,9 +33,9 @@ class KanaKanjierUITests: XCTestCase {
     }
 
     func testLaunchPerformance() throws {
-        if #available(macOS 10.15, iOS 13.0, tvOS 13.0, *) {
+        if #available(macOS 10.15, iOS 14.0, tvOS 13.0, *) {
             // This measures how long it takes to launch your application.
-            measure(metrics: [XCTOSSignpostMetric.applicationLaunch]) {
+            measure(metrics: [XCTApplicationLaunchMetric()]) {
                 XCUIApplication().launch()
             }
         }
