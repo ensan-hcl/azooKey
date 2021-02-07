bf0aa0be37b7c8fe020cbb3b600d553fb95f7411
En3_HCl <En3_HCl_K@icloud.com>
2021-02-08 01:08:39 +0900

    テーマの選択画面を追加

--- KanaKanjier/Theme/ThemeTab.swift
+++ KanaKanjier/Theme/ThemeTab.swift
@@ -10,6 +10,7 @@ import SwiftUI
 
 struct ThemeTabView: View {
     @ObservedObject private var storeVariableSection = Store.variableSection
+    @State private var selection = 0
 
     var body: some View {
         NavigationView {
@@ -17,9 +18,30 @@ struct ThemeTabView: View {
                 Section(header: Text("作る")){
                     NavigationLink("テーマを作成", destination: ThemeEditView())
                 }
+                Section(header: Text("選ぶ")){
+                    LazyVGrid(columns: Array(repeating: GridItem(), count: 2)) { // カラム数の指定
+                        ForEach((1...10), id: \.self) { index in
+                            Image("themeImage0")
+                                .resizable()
+                                .scaledToFit()
+                                .overlay(
+                                    Group{
+                                        if selection == index{
+                                            Image(systemName: "checkmark.circle.fill")
+                                                .renderingMode(.original)
+                                                .font(.system(size: 50))
+                                        }
+                                    }
+                                )
+                                .onTapGesture {
+                                    selection = index
+                                }
+                        }
+                    }
+                }
             }
             .navigationBarTitle(Text("着せ替え"), displayMode: .large)
-                    }
+        }
         .navigationViewStyle(StackNavigationViewStyle())
         .font(.body)
     }
