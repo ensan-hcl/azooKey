c25a5e65be82d6eac4ff98623310e92141567e62
En3_HCl <En3_HCl_K@icloud.com>
2021-02-08 00:25:21 +0900

    画像を使えるように変更。データの保存処理があるので後から作り込む

--- Shared/ThemeData/ThemeData.swift
+++ Shared/ThemeData/ThemeData.swift
@@ -22,6 +22,83 @@ struct ThemeData: Codable, Equatable {
     static let mock: Self = Self.init(pictureFileName: "wallPaperMock", textColor: Color(.displayP3, white: 1, opacity: 1), textFont: .bold, resultTextColor: Color(.displayP3, white: 1, opacity: 1), borderColor: Color(.displayP3, white: 0, opacity: 0), keyBackgroundColorOpacity: 0.3)
 
     static let clear: Self = Self.init(pictureFileName: "wallPaperMock", textColor: Color(.displayP3, white: 1, opacity: 1), textFont: .bold, resultTextColor: Color(.displayP3, white: 1, opacity: 1), borderColor: Color(.displayP3, white: 1, opacity: 0), keyBackgroundColorOpacity: 0.001)
+}
+
+enum ThemePicture: Codable {
+    case none
+    case path(String)
+    case asset(String)
+    case uiImage(UIImage)
+
+    var image: Image? {
+        switch self{
+        case .none:
+            return nil
+        case let .path(path):
+            let data = Data()   //この部分でpathから画像を読み込む
+            return UIImage(data: data).flatMap{Image(uiImage: $0)}
+        case let .asset(name):
+            return Image(name)
+        case let .uiImage(uiImage):
+            return Image(uiImage: uiImage)
+        }
+    }
+
+    enum DecodeError: Error {
+        case emptyPath
+    }
+
+    public init(from decoder: Decoder) throws {
+        let values = try decoder.container(keyedBy: CodingKeys.self)
+        let valueType = try values.decode(ValueType.self, forKey: .value)
+        let value = try values.decode(String?.self, forKey: .value)
+        switch valueType{
+        case .none:
+            self = .none
+        case .path:
+            guard let path = value else{ throw DecodeError.emptyPath }
+            self = .path(path)
+        case .asset:
+            guard let name = value else{ throw DecodeError.emptyPath }
+            self = .asset(name)
+        }
+    }
+
+    private enum ValueType: String, Codable {
+        case none
+        case path
+        case asset
+    }
+
+    enum CodingKeys: String, CodingKey {
+        case valueType
+        case value
+    }
+
+    public func encode(to encoder: Encoder) throws {
+        var container = encoder.container(keyedBy: CodingKeys.self)
+
+        let valueType: ValueType
+        let value: String?
+        switch self{
+        case .none:
+            valueType = .none
+            value = nil
+        case let .path(path):
+            valueType = .path
+            value = path
+        case let .asset(name):
+            valueType = .asset
+            value = name
+        case let .uiImage(uiImage):
+            //ここでuiImageを保存する
+            valueType = .path
+            value = "path"
+        }
+
+        try container.encode(valueType, forKey: .valueType)
+        try container.encode(value, forKey: .value)
+    }
 
 }
 
@@ -44,7 +121,7 @@ extension Color: Codable {
         case blue
         case opacity
     }
-    
+
     public init(from decoder: Decoder) throws {
         let values = try decoder.container(keyedBy: CodingKeys.self)
         let red = try values.decode(Double.self, forKey: .red)
