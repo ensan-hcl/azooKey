646eb356d1ec650340eede98b858e3a2b844557d
En3_HCl <En3_HCl_K@icloud.com>
2021-03-13 22:55:29 +0900

    片手モードについてのtipsを追加

---
+++ KanaKanjier/Tips/Articles/OneHandedModeTips.swift
@@ -0,0 +1,41 @@
+//
+//  OneHandedModeTips.swift
+//  KanaKanjier
+//
+//  Created by β α on 2021/03/13.
+//  Copyright © 2021 DevEn3. All rights reserved.
+//
+
+import Foundation
+import SwiftUI
+
+struct OneHandedModeTipsView: View {
+    var body: some View {
+        TipsContentView("片手モードを利用する"){
+            TipsContentParagraph{
+                Text("azooKeyでは片手モードを利用可能です。")
+            }
+            TipsContentParagraph{
+                Text("まずタブバーを表示します。変換候補欄を長押しするか記号タブキーを長押しすると表示されます。")
+                Text("「片手」をタップします。")
+                TipsImage("oneHandedMode_1")
+            }
+            TipsContentParagraph{
+                Text("サイズの調整モードがオンになります。左右の白い部分をドラッグし、好みのサイズに調整してください。")
+                TipsImage("oneHandedMode_2")
+            }
+            TipsContentParagraph{
+                Text("\(systemImage: "checkmark")をタップすると完了します。")
+                Text("\(systemImage: "arrow.triangle.2.circlepath")をタップするとリセットされます。")
+                TipsImage("oneHandedMode_3")
+            }
+
+            TipsContentParagraph{
+                Text("\(systemImage: "checkmark")を押して完了したあとは通常のキーボードと同様に使えます。")
+                Text("\(systemImage: "aspectratio")を押すと再度編集できます。")
+                Text("\(systemImage: "arrow.up.backward.and.arrow.down.forward")を押すと通常の両手モードに戻ります。")
+                TipsImage("oneHandedMode_4")
+            }
+        }
+    }
+}
--- KanaKanjier/Tips/TipsView.swift
+++ KanaKanjier/Tips/TipsView.swift
@@ -27,69 +27,50 @@ struct TipsTabView: View {
                                 }
                         }
                         NavigationLink(destination: SelctInputStyleTipsView()) {
-                            VStack{
-                                Text("入力方法を選ぶ")
-                            }
+                            Text("入力方法を選ぶ")
                         }
                     }
 
                     Section(header: Text("便利な使い方")){
+                        NavigationLink(destination: OneHandedModeTipsView()) {
+                            Text("片手モードを使う")
+                        }
                         NavigationLink(destination: CursorMoveTipsView()) {
-                            VStack{
-                                Text("カーソルを自由に移動する")
-                            }
+                            Text("カーソルを自由に移動する")
                         }
                         NavigationLink(destination: SmoothDeleteTipsView()) {
-                            VStack{
-                                Text("文頭まで一気に消す")
-                            }
+                            Text("文頭まで一気に消す")
                         }
                         NavigationLink(destination: KanjiLargeTextTipsView()) {
-                            VStack{
-                                Text("漢字を拡大表示する")
-                            }
+                            Text("漢字を拡大表示する")
                         }
                         NavigationLink(destination: CapsLockTipsView()) {
-                            VStack{
-                                Text("大文字に固定する")
-                            }
+                            Text("大文字に固定する")
                         }
                         NavigationLink(destination: TemplateSettingTipsView()) {
-                            VStack{
-                                Text("タイムスタンプを使う")
-                            }
+                            Text("タイムスタンプを使う")
                         }
                         NavigationLink(destination: CustomKeyTipsView()) {
-                            VStack{
-                                Text("キーをカスタマイズする")
-                            }
+                            Text("キーをカスタマイズする")
                         }
                     }
 
                     Section(header: Text("困ったときは")){
                         NavigationLink(destination: DynamicTypeSettingFailureTipsView()) {
-                            VStack{
-                                Text("端末の文字サイズ設定が反映されない")
-                            }
+                            Text("端末の文字サイズ設定が反映されない")
                         }
                         NavigationLink(destination: EmojiKaomojiTipsView()) {
-                            VStack{
-                                Text("絵文字や顔文字の変換候補を表示したい")
-                            }
+                            Text("絵文字や顔文字の変換候補を表示したい")
                         }
                         NavigationLink(destination: ContactView()) {
-                            VStack{
-                                Text("バグの報告や機能のリクエストをしたい")
-                            }
+                            Text("バグの報告や機能のリクエストをしたい")
                         }
 
                     }
                 }
                 .navigationBarTitle(Text("使い方"), displayMode: .large)
-
             }
             .navigationViewStyle(StackNavigationViewStyle())
-
         }
     }
 }
