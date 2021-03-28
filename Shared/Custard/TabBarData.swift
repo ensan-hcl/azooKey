e41b9d3f78724cdd678f2768586591059e7c02d1
En3_HCl <En3_HCl_K@icloud.com>
2021-03-28 14:28:54 +0900

    Encode処理の改善

--- Shared/Custard/TabBarData.swift
+++ Shared/Custard/TabBarData.swift
@@ -19,52 +19,44 @@ enum TabBarItemLabelType: Codable, Equatable {
 }
 
 extension TabBarItemLabelType{
-    enum CodingKeys: CodingKey{
+    private enum CodingKeys: CodingKey{
         case text
         case imageAndText
         case image
     }
 
+    private var key: CodingKeys {
+        switch self{
+        case .image: return .image
+        case .text: return .text
+        case .imageAndText: return .imageAndText
+        }
+    }
+
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         switch self {
-        case let .text(value):
-            try container.encode(value, forKey: .text)
-        case let .imageAndText(value):
-            try container.encode(value, forKey: .imageAndText)
-        case let .image(value):
-            try container.encode(value, forKey: .image)
+        case let .text(value as Encodable), let .imageAndText(value as Encodable), let .image(value as Encodable):
+            try value.containerEncode(container: &container, key: self.key)
         }
     }
 
     init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
-        guard let key = container.allKeys.first else{
+        guard let key = container.allKeys.first else {
             throw DecodingError.dataCorrupted(
-                DecodingError.Context(
-                    codingPath: container.codingPath,
-                    debugDescription: "Unabled to decode TabBarItemLabelType."
-                )
+                DecodingError.Context(codingPath: container.codingPath, debugDescription: "Unabled to decode TabBarItemLabelType.")
             )
         }
         switch key {
         case .text:
-            let value = try container.decode(
-                String.self,
-                forKey: .text
-            )
+            let value = try container.decode(String.self,forKey: .text)
             self = .text(value)
         case .imageAndText:
-            let value = try container.decode(
-                ImageAndText.self,
-                forKey: .imageAndText
-            )
+            let value = try container.decode(ImageAndText.self,forKey: .imageAndText)
             self = .imageAndText(value)
         case .image:
-            let value = try container.decode(
-                String.self,
-                forKey: .image
-            )
+            let value = try container.decode(String.self,forKey: .image)
             self = .image(value)
         }
     }
