a8290184c45d1260f076426a17b221c4bf0d0457
En3_HCl <En3_HCl_K@icloud.com>
2021-04-01 13:05:17 +0900

    use bullet

--- KanaKanjier/Customize/EditingScrollCustardView.swift
+++ KanaKanjier/Customize/EditingScrollCustardView.swift
@@ -23,7 +23,7 @@ fileprivate extension CustardInterfaceLayoutScrollValue.ScrollDirection{
 struct EditingScrollCustardView: View {
     private let base: [CustardKeyPositionSpecifier: CustardInterfaceKey] = [
         .gridScroll(0): .system(.changeKeyboard),
-        .gridScroll(1): .custom(.init(design: .init(label: .systemImage("list.dash"), color: .special), press_actions: [.toggleTabBar], longpress_actions: .none, variations: [])),
+        .gridScroll(1): .custom(.init(design: .init(label: .systemImage("list.bullet"), color: .special), press_actions: [.toggleTabBar], longpress_actions: .none, variations: [])),
         .gridScroll(2): .custom(.init(design: .init(label: .systemImage("delete.left"), color: .special), press_actions: [.delete(1)], longpress_actions: .init(repeat: [.delete(1)]), variations: [])),
         .gridScroll(3): .system(.enter),
     ]
