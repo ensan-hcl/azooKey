966b242dd78e2355bac514a1692aef8b9a44d7b3
En3_HCl <En3_HCl_K@icloud.com>
2021-04-23 23:18:38 +0900

    本当に必要なとき以外ウォークスルーが表示されないように変更

--- KanaKanjier/Customize/CustomizeTabWalkthrough.swift
+++ KanaKanjier/Customize/CustomizeTabWalkthrough.swift
@@ -43,64 +43,66 @@ struct CustomizeTabWalkthroughView: View {
     ]
 
     var body: some View {
-        GeometryReader {geometry in
-            ScrollView {
-                VStack(spacing: 20) {
-                    // アイコンの辺の長さ
-                    let length = geometry.size.width / 4.8
-                    Image(systemName: "gearshape.2.fill")
-                        .font(.system(size: length / 2, weight: .bold, design: .default))
-                        .background(
-                            RoundedRectangle(cornerRadius: length * 0.17)
-                                .fill(Color.systemGray5)
-                                .frame(width: length, height: length)
-                        )
-                    Text("azooKeyを拡張する").font(.largeTitle.bold())
-                        .padding()
-                    let imagesFont: Font = Font.system(size: length / 2.4, weight: .light, design: .default)
-                    ForEach(items) {item in
-                        HStack {
-                            Image(systemName: item.image)
-                                .font(imagesFont)
-                                .frame(width: geometry.size.width / 7.0, height: geometry.size.width / 7.0)
-                                .foregroundColor(.blue)
-                            VStack(alignment: .leading) {
-                                Text(item.headline)
-                                    .font(.subheadline.bold())
-                                Text(item.body)
-                                    .foregroundColor(.gray)
-                                    .font(.subheadline)
-                            }
-                        }
-                        .padding(.horizontal, 20)
-                        .lineLimit(nil)
-                        .fixedSize(horizontal: false, vertical: true)
-                    }
-
-                    Button {
-                        isShowing = false
-                    }label: {
-                        Text("始める")
-                            .bold()
-                            .padding()
-                            .frame(width: geometry.size.width * 0.9)
-                            .foregroundColor(.background)
+        if ContainerInternalSetting.shared.walkthroughState.shouldDisplay(identifier: .extensions) {
+            GeometryReader {geometry in
+                ScrollView {
+                    VStack(spacing: 20) {
+                        // アイコンの辺の長さ
+                        let length = geometry.size.width / 4.8
+                        Image(systemName: "gearshape.2.fill")
+                            .font(.system(size: length / 2, weight: .bold, design: .default))
                             .background(
                                 RoundedRectangle(cornerRadius: length * 0.17)
-                                    .fill(Color.blue)
+                                    .fill(Color.systemGray5)
+                                    .frame(width: length, height: length)
                             )
-                            .padding(.top, 30)
+                        Text("azooKeyを拡張する").font(.largeTitle.bold())
+                            .padding()
+                        let imagesFont: Font = Font.system(size: length / 2.4, weight: .light, design: .default)
+                        ForEach(items) {item in
+                            HStack {
+                                Image(systemName: item.image)
+                                    .font(imagesFont)
+                                    .frame(width: geometry.size.width / 7.0, height: geometry.size.width / 7.0)
+                                    .foregroundColor(.blue)
+                                VStack(alignment: .leading) {
+                                    Text(item.headline)
+                                        .font(.subheadline.bold())
+                                    Text(item.body)
+                                        .foregroundColor(.gray)
+                                        .font(.subheadline)
+                                }
+                            }
+                            .padding(.horizontal, 20)
+                            .lineLimit(nil)
+                            .fixedSize(horizontal: false, vertical: true)
+                        }
+
+                        Button {
+                            isShowing = false
+                        }label: {
+                            Text("始める")
+                                .bold()
+                                .padding()
+                                .frame(width: geometry.size.width * 0.9)
+                                .foregroundColor(.background)
+                                .background(
+                                    RoundedRectangle(cornerRadius: length * 0.17)
+                                        .fill(Color.blue)
+                                )
+                                .padding(.top, 30)
+                        }
                     }
+                    .background(Color.background)
+                    .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
                 }
-                .background(Color.background)
-                .position(x: geometry.size.width / 2, y: geometry.size.height / 2)
             }
-        }
-        .onChange(of: isShowing) {value in
-            // コードの明確化のためにfalseと比較している
-            if value == false {
-                ContainerInternalSetting.shared.update(\.walkthroughState) {value in
-                    value.done(identifier: .extensions)
+            .onChange(of: isShowing) {value in
+                // コードの明確化のためにfalseと比較している
+                if value == false {
+                    ContainerInternalSetting.shared.update(\.walkthroughState) {value in
+                        value.done(identifier: .extensions)
+                    }
                 }
             }
         }
