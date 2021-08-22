efba44248583c5d2ee7552c05bcba0eca9405a9d
En3_HCl <En3_HCl_K@icloud.com>
2021-08-22 11:23:01 +0900

    UIKit AppからSwiftUI Appに移行

---
+++ KanaKanjier/App.swift
@@ -0,0 +1,19 @@
+//
+//  App.swift
+//  App
+//
+//  Created by β α on 2021/08/22.
+//  Copyright © 2021 DevEn3. All rights reserved.
+//
+
+import SwiftUI
+
+@main
+struct MainApp: App {
+    @UIApplicationDelegateAdaptor var delegate: AppDelegate
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
--- KanaKanjier/AppDelegate.swift
+++ KanaKanjier/AppDelegate.swift
@@ -8,7 +8,6 @@
 
 import UIKit
 
-@UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
 
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
