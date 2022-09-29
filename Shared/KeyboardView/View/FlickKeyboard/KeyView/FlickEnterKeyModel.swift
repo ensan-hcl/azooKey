a0e0821183a170db06165fcad207261a59259d5a
ensan <ensan.hcl.dev@gmail.com>
2022-09-29 12:58:43 +0900

    letで宣言

--- Shared/KeyboardView/View/FlickKeyboard/KeyView/FlickEnterKeyModel.swift
+++ Shared/KeyboardView/View/FlickKeyboard/KeyView/FlickEnterKeyModel.swift
@@ -10,7 +10,7 @@ import Foundation
 import SwiftUI
 
 struct FlickEnterKeyModel: FlickKeyModelProtocol {
-    static var shared = FlickEnterKeyModel()
+    static let shared = FlickEnterKeyModel()
     let suggestModel = SuggestModel([:])
     let needSuggestView = false
 
