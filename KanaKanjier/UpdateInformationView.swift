a25275cfe807741086f1e4120351912e81c39019
ensan <ensan.hcl.dev@gmail.com>
2022-11-01 00:38:11 +0900

    1.9.1の修正内容を追加

--- KanaKanjier/UpdateInformationView.swift
+++ KanaKanjier/UpdateInformationView.swift
@@ -12,7 +12,12 @@ struct UpdateInfomationView: View {
         Form {
             // version 1.9系
             Group {
-                VersionView("1.9", releaseDate: "2022年11月01日") {
+                VersionView("1.9.1", releaseDate: "2022年11月01日") {
+                    ParagraphView("不具合を修正しました") {
+                        "iPadOS 15で複数の日本語タブが存在する場合にキーボードの種類の選択ができなくなる問題を修正"
+                    }
+                }
+                VersionView("1.9", releaseDate: "2022年10月31日") {
                     ParagraphView("学習機能を大幅に改善しました。") {
                         "学習結果がより長い期間にわたり維持されるようになりました"
                         "実験的な機能として、さらに長期間にわたって学習結果を維持するオプションも導入しました"
