a2902fb0e47bfee268161a96fb709c24244437e6
ensan <ensan.hcl.dev@gmail.com>
2022-12-26 01:29:01 +0900

    Email変換で`@`だけ入力してもアドレスが出るように変更

---
+++ KanaKanjierTests/KeyboardTests/EmailAddressConversionTests.swift
@@ -0,0 +1,56 @@
+//
+//  EmailAddressConversionTests.swift
+//  KanaKanjierTests
+//
+//  Created by β α on 2022/12/26.
+//  Copyright © 2022 DevEn3. All rights reserved.
+//
+
+import XCTest
+
+final class EmailAddressConversionTests: XCTestCase {
+    func makeDirectInput(direct input: String) -> ComposingText {
+        ComposingText(
+            convertTargetCursorPosition: input.count,
+            input: input.map {.init(character: $0, inputStyle: .direct)},
+            convertTarget: input
+        )
+    }
+
+    func testToEmailAddress() throws {
+        do {
+            let converter = KanaKanjiConverter()
+            let input = makeDirectInput(direct: "azooKey@")
+            let result = converter.toEmailAddress(input)
+            XCTAssertFalse(result.isEmpty)
+            XCTAssertTrue(result.contains(where: {$0.text == "azooKey@gmail.com"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "azooKey@icloud.com"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "azooKey@yahoo.co.jp"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "azooKey@i.softbank.jp"}))
+        }
+
+        do {
+            let converter = KanaKanjiConverter()
+            let input = makeDirectInput(direct: "my.dev_az@")
+            let result = converter.toEmailAddress(input)
+            XCTAssertFalse(result.isEmpty)
+            XCTAssertTrue(result.contains(where: {$0.text == "my.dev_az@gmail.com"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "my.dev_az@icloud.com"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "my.dev_az@yahoo.co.jp"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "my.dev_azc@i.softbank.jp"}))
+        }
+
+        do {
+            let converter = KanaKanjiConverter()
+            let input = makeDirectInput(direct: "@")
+            let result = converter.toEmailAddress(input)
+            XCTAssertFalse(result.isEmpty)
+            XCTAssertTrue(result.contains(where: {$0.text == "@gmail.com"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "@icloud.com"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "@yahoo.co.jp"}))
+            XCTAssertTrue(result.contains(where: {$0.text == "@i.softbank.jp"}))
+        }
+
+    }
+
+}
--- Keyboard/Main/Converter/Converter/EmailAddress.swift
+++ Keyboard/Main/Converter/Converter/EmailAddress.swift
@@ -35,17 +35,21 @@ extension KanaKanjiConverter {
     /// 入力が@で終わる場合に、メアドのような候補を追加する関数
     /// - parameters:
     func toEmailAddress(_ inputData: ComposingText) -> [Candidate] {
-        let baseValue: PValue = -13
-        let string = inputData.convertTarget.toKatakana()
-        if inputData.convertTarget.last != "@" || !inputData.convertTarget.dropLast(1).isEnglishSentence {
+        if !inputData.convertTarget.hasSuffix("@") {
             return []
         }
+        let id = inputData.convertTarget.dropLast(1)
+        if !(id.isEnglishSentence || id.isEmpty) {
+            return []
+        }
+        let baseValue: PValue = id.isEmpty ? -20 : -13
+        let string = inputData.convertTarget.toKatakana()
         var results: [Candidate] = []
         for (i, domain) in Self.domains.enumerated() {
-            let address = inputData.convertTarget.dropLast(1).appending(domain)
+            let address = id.appending(domain)
             results.append(
                 Candidate(
-                    text: String(address),
+                    text: address,
                     value: baseValue - PValue(i),
                     correspondingCount: inputData.input.count,
                     lastMid: MIDData.一般.mid,
