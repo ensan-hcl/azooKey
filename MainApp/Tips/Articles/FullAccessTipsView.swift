a8420f8e1c96fcc195713a6a1b97144fc987060c
ensan <ensan.hcl.dev@gmail.com>
2023-03-06 01:21:12 +0900

    フルアクセスについて説明するTipsを追加

---
+++ MainApp/Tips/Articles/FullAccessTipsView.swift
@@ -0,0 +1,45 @@
+//
+//  FullAccessTipsView.swift
+//  azooKey
+//
+//  Created by ensan on 2023/03/06.
+//  Copyright © 2023 ensan. All rights reserved.
+//
+
+import SwiftUI
+
+struct FullAccessTipsView: View {
+    var body: some View {
+        TipsContentView("フルアクセスについて") {
+            TipsContentParagraph {
+                Text("azooKeyは、キーボードの振動フィードバックや、キーボードからのペーストを実現するために、フルアクセスの許可が必要です。フルアクセスを許可していただくことで、これらの機能が使用できます。")
+            }
+            TipsContentParagraph {
+                Text("以下は、フルアクセスをオンにすることで利用できる設定項目です。フルアクセスを必要とする機能は、すべてデフォルトで無効になっているため、フルアクセスを許可しただけでキーボードの振る舞いが変わることはありません。")
+                Text("また、フルアクセスをオンにしたあとでも、いつでもフルアクセスをオフにすることができます。")
+            }
+            BoolSettingView(.enableKeyHaptics)
+            BoolSettingView(.enablePasteButton)
+            BoolSettingView(.enableClipboardHistoryManagerTab)
+
+            TipsContentParagraph {
+                Text("フルアクセスを許可しなくても、キーボードは完全に動作します。上記の機能を使わない場合は、オンにしないことを強くおすすめします。")
+            }
+            TipsContentParagraph {
+                Text("なお、azooKeyはオープンソースであり、誰もが実装を確認することができます。")
+                Text("azooKeyは安全なキーボードアプリであり、ユーザの明示的な同意なく、個人情報を外部に送信したり、保存したりすることはありません。")
+            }
+            if let url = URL(string: UIApplication.openSettingsURLString) {
+                Button(Store.shared.isFullAccessEnabled
+                       ? "フルアクセスをオフにする（設定アプリが開きます）"
+                       : "フルアクセスをオンにする（設定アプリが開きます）") {
+                    UIApplication.shared.open(url, options: [:], completionHandler: nil)
+                }
+            }
+            Section("参考情報") {
+                FallbackLink("azooKeyのソースコードを閲覧する", destination: URL(string: "https://github.com/ensan-hcl/azooKey/tree/main")!)
+                FallbackLink("Configuring open access for a custom keyboard | Apple Developer Documentation (英語)", destination: URL(string: "https://developer.apple.com/documentation/uikit/keyboards_and_input/creating_a_custom_keyboard/configuring_open_access_for_a_custom_keyboard")!)
+            }
+        }
+    }
+}
