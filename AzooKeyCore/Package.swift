736f0be8d2211991b0abee8fd9118dace265897a
ensan <ensan.hcl.dev@gmail.com>
2023-05-28 11:51:25 +0900

    Rename KanaKanjiConverterModule into AzooKeyCore

--- AzooKeyCore/Package.swift
+++ AzooKeyCore/Package.swift
@@ -4,7 +4,7 @@
 import PackageDescription
 
 let package = Package(
-    name: "KanaKanjiConverterModule",
+    name: "AzooKeyCore",
     platforms: [.iOS(.v14), .macOS(.v11)],
     products: [
         // Products define the executables and libraries a package produces, and make them visible to other packages.
