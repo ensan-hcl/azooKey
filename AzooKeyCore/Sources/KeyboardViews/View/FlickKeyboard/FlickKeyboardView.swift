8c335f86f8d19e733197a352bc70bf7f135c179a
ensan-hcl <ensan.hcl.dev@gmail.com>
2023-09-09 18:44:40 +0900

    FlickKeyboardViewの実装をCustardFlickKeysViewで置き換え

--- AzooKeyCore/Sources/KeyboardViews/View/FlickKeyboard/FlickKeyboardView.swift
+++ AzooKeyCore/Sources/KeyboardViews/View/FlickKeyboard/FlickKeyboardView.swift
@@ -14,63 +14,24 @@ struct FlickKeyboardView<Extension: ApplicationSpecificKeyboardViewExtension>: V
     @State private var suggestState = FlickSuggestState()
 
     private let tabDesign: TabDependentDesign
-    private let keyModels: [[any FlickKeyModelProtocol]]
+    private let models: [KeyPosition: (model: any FlickKeyModelProtocol, width: Int, height: Int)]
     init(keyModels: [[any FlickKeyModelProtocol]], interfaceSize: CGSize, keyboardOrientation: KeyboardOrientation) {
-        self.keyModels = keyModels
         self.tabDesign = TabDependentDesign(width: 5, height: 4, interfaceSize: interfaceSize, layout: .flick, orientation: keyboardOrientation)
-    }
-
-    private var horizontalIndices: Range<Int> {
-        keyModels.indices
-    }
-
-    private func verticalIndices(h: Int) -> Range<Int> {
-        keyModels[h].indices
-    }
-
-    @MainActor private func keyView(h: Int, v: Int) -> FlickKeyView<Extension> {
-        let model = self.keyModels[h][v]
-        let size: CGSize
-        if model is FlickEnterKeyModel<Extension> {
-            size = CGSize(width: tabDesign.keyViewWidth, height: tabDesign.keyViewHeight(heightCount: 2))
-        } else {
-            size = tabDesign.keyViewSize
+        
+        var models: [KeyPosition: (model: any FlickKeyModelProtocol, width: Int, height: Int)] = [:]
+        for h in keyModels.indices {
+            for v in keyModels[h].indices {
+                let model = keyModels[h][v]
+                models[KeyPosition.gridFit(x: h, y: v)] = (keyModels[h][v], 1, model is FlickEnterKeyModel<Extension> ? 2 : 1)
+            }
         }
-        return FlickKeyView<Extension>(model: model, size: size, position: (x: h, y: v), suggestState: $suggestState)
-    }
-
-    @MainActor
-    private func suggestView(h: Int, v: Int, suggestType: FlickSuggestType) -> FlickSuggestView<Extension> {
-        let model = self.keyModels[h][v]
-        let size: CGSize
-        if model is FlickEnterKeyModel<Extension> {
-            size = CGSize(width: tabDesign.keyViewWidth, height: tabDesign.keyViewHeight(heightCount: 2))
-        } else {
-            size = tabDesign.keyViewSize
-        }
-        return FlickSuggestView<Extension>(model: model, tabDesign: tabDesign, size: size, suggestType: suggestType)
+        self.models = models
     }
 
     var body: some View {
-        ZStack {
-            HStack(spacing: tabDesign.horizontalSpacing) {
-                ForEach(self.horizontalIndices, id: \.self) {h in
-                    let columnSuggestStates = self.suggestState.items[h, default: [:]]
-                    VStack(spacing: tabDesign.verticalSpacing) {
-                        ForEach(self.verticalIndices(h: h), id: \.self) {v in
-                            self.keyView(h: h, v: v)
-                                .zIndex(columnSuggestStates[v] != nil ? 1 : 0)
-                                .overlay(alignment: .center) {
-                                    if let suggestType = columnSuggestStates[v] {
-                                        suggestView(h: h, v: v, suggestType: suggestType)
-                                            .zIndex(2)
-                                    }
-                                }
-                        }
-                    }
-                    .zIndex(columnSuggestStates.isEmpty ? 0 : 1)
-                }
-            }
+        let layout = CustardInterfaceLayoutGridValue(rowCount: Int(tabDesign.horizontalKeyCount), columnCount: Int(tabDesign.verticalKeyCount))
+        CustardFlickKeysView(models: models, tabDesign: tabDesign, layout: layout) {(view: FlickKeyView<Extension>, _, _) in
+            view
         }
     }
 }
