f199485d1894b7cc7224af12b316e059d777d50c
ensan-hcl <ensan.hcl.dev@gmail.com>
2023-09-30 10:19:49 +0900

    Improve operation fluency

--- AzooKeyCore/Sources/KeyboardViews/View/KeyboardBar/SliderStyleCursorBar.swift
+++ AzooKeyCore/Sources/KeyboardViews/View/KeyboardBar/SliderStyleCursorBar.swift
@@ -13,43 +13,71 @@ import SwiftUtils
 
 @MainActor
 struct SliderStyleCursorBar<Extension: ApplicationSpecificKeyboardViewExtension>: View {
-    private enum MoveCursorBarGestureState {
+    enum SwipeGestureState {
         case inactive
-        case moving(CGPoint, Int)   // 右だったら+1、左だったら-1
+        case start(l1: CGPoint, l2: CGPoint, l3: CGPoint)
+        case moving(l1: CGPoint, l2: CGPoint, l3: CGPoint, count: Double)
     }
 
     init() {}
 
     @EnvironmentObject private var variableStates: VariableStates
-    @State private var gestureState: MoveCursorBarGestureState = .inactive
+    @State private var swipeGestureState: SwipeGestureState = .inactive
     @Environment(Extension.Theme.self) private var theme
     @Environment(\.userActionManager) private var action
 
-    private var gesture: some Gesture {
-        DragGesture(minimumDistance: 0)
+    var swipeGesture: some Gesture {
+        DragGesture(minimumDistance: 0, coordinateSpace: .global)
             .onChanged {value in
-                switch self.gestureState {
+                switch swipeGestureState {
                 case .inactive:
-                    self.gestureState = .moving(value.location, 0)
-                case let .moving(previous, count):
-                    let dx = (value.location.x - previous.x)
-                    if dx.isZero {
-                        break
-                    }
-                    let newCount = count + Int(dx / abs(dx))
-                    if newCount > 1 {
-                        self.gestureState = .moving(value.location, 0)
-                        self.action.registerAction(.moveCursor(1), variableStates: variableStates)
-                    } else if newCount < -1 {
-                        self.gestureState = .moving(value.location, 0)
-                        self.action.registerAction(.moveCursor(-1), variableStates: variableStates)
+                    swipeGestureState = .start(l1: value.location, l2: value.location, l3: value.location)
+                case let .start(l1, l2, _):
+                    let d = value.startLocation.distance(to: value.location)
+                    if d > 20 {
+                        swipeGestureState = .moving(l1: value.location, l2: l1, l3: l2, count: 0)
                     } else {
-                        self.gestureState = .moving(value.location, newCount)
+                        swipeGestureState = .start(l1: value.location, l2: l1, l3: l2)
                     }
+                case let .moving(l1, l2, l3, count):
+                    var direction = 0
+                    var count = count
+                    // directionを多数決で決定する
+                    if value.location.x - l1.x > 0 {
+                        direction -= 1
+                    } else {
+                        direction += 1
+                    }
+                    if l1.x - l2.x > 0 {
+                        direction -= 1
+                    } else {
+                        direction += 1
+                    }
+                    if l2.x - l3.x > 0 {
+                        direction -= 1
+                    } else {
+                        direction += 1
+                    }
+                    // countの更新
+                    if direction > 0 && value.location.x < l3.x {
+                        count += (Double(direction) / 3) * (l3.x - value.location.x) / 3
+                    } else if direction < 0 && value.location.x > l3.x {
+                        count -= (Double(direction) / 3) * (l3.x - value.location.x) / 3
+                    }
+                    let threshlod: Double = 12
+                    if count >= threshlod {
+                        action.registerAction(.moveCursor(-1), variableStates: variableStates)
+                        count -= threshlod
+                    }
+                    if count <= -threshlod {
+                        action.registerAction(.moveCursor(1), variableStates: variableStates)
+                        count += threshlod
+                    }
+                    swipeGestureState = .moving(l1: value.location, l2: l1, l3: l2, count: count)
                 }
             }
-            .onEnded {_ in
-                self.gestureState = .inactive
+            .onEnded {value in
+                swipeGestureState = .inactive
             }
     }
 
@@ -69,28 +97,36 @@ struct SliderStyleCursorBar<Extension: ApplicationSpecificKeyboardViewExtension>
         theme.resultTextColor.color
     }
 
+    @ViewBuilder private var moveLeftButton: some View {
+        Button {
+            self.action.registerAction(.moveCursor(-1), variableStates: variableStates)
+        } label: {
+            Image(systemName: "chevron.left.2").font(.system(size: 18, weight: symbolsFontWeight, design: .default))
+                .padding()
+        }
+    }
+
+    @ViewBuilder private var moveRightButton: some View {
+        Button {
+            self.action.registerAction(.moveCursor(1), variableStates: variableStates)
+        } label: {
+            Image(systemName: "chevron.right.2").font(.system(size: 18, weight: symbolsFontWeight, design: .default))
+                .padding()
+        }
+    }
+
     var body: some View {
         RadialGradient(gradient: Gradient(colors: [centerColor, edgeColor]), center: .center, startRadius: 1, endRadius: 200)
             .cornerRadius(20)
-            .gesture(gesture)
+            .gesture(swipeGesture)
             .overlay(
                 HStack {
                     Spacer()
-                    Button {
-                        self.action.registerAction(.moveCursor(-1), variableStates: variableStates)
-                    } label: {
-                        Image(systemName: "chevron.left.2").font(.system(size: 18, weight: symbolsFontWeight, design: .default))
-                            .padding()
-                    }
+                    moveLeftButton
                     Spacer()
                     Image(systemName: "circle.fill").font(.system(size: 22, weight: symbolsFontWeight, design: .default))
                     Spacer()
-                    Button {
-                        self.action.registerAction(.moveCursor(1), variableStates: variableStates)
-                    } label: {
-                        Image(systemName: "chevron.right.2").font(.system(size: 18, weight: symbolsFontWeight, design: .default))
-                            .padding()
-                    }
+                    moveRightButton
                     Spacer()
                 }
                     .foregroundStyle(symbolsColor)
