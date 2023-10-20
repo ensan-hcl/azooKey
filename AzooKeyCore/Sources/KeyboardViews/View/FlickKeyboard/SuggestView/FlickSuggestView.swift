20c3093dfd3c71a1d6c902526a1e457ff3af872f
ensan-hcl <ensan.hcl.dev@gmail.com>
2023-10-20 10:30:08 +0900

    adjust suggest positioning

--- AzooKeyCore/Sources/KeyboardViews/View/FlickKeyboard/SuggestView/FlickSuggestView.swift
+++ AzooKeyCore/Sources/KeyboardViews/View/FlickKeyboard/SuggestView/FlickSuggestView.swift
@@ -51,58 +51,45 @@ struct FlickSuggestView<Extension: ApplicationSpecificKeyboardViewExtension>: Vi
         let color = isPointed ? pointedColor : unpointedColor
         let shape: any Shape
         let paddings: [CGFloat]
-        let spacers: [CGFloat]
         let sizeTimes: [CGFloat]
         
         switch direction {
         case .top:
             shape = RoundedPentagonTop()
-            paddings = [0, 0, 0.2, 0]
-            spacers = [0, 0, 0, 0.25]
+            paddings = [0, 0, 0.3, 0]
             sizeTimes = [1.2, 1.5]
         case .left:
             shape = RoundedPentagonLeft()
             paddings = [0, 0, 0, 0.3]
-            spacers = [0, 0, 0.1, 0]
             sizeTimes = [1.5, 1.2]
         case .right:
             shape = RoundedPentagonRight()
             paddings = [0, 0.3, 0, 0]
-            spacers = [0, 0.1, 0, 0]
             sizeTimes = [1.5, 1.2]
         case .bottom:
             shape = RoundedPentagonBottom()
-            paddings = [0.2, 0, 0, 0]
-            spacers = [0.25, 0, 0, 0]
+            paddings = [0.3, 0, 0, 0]
             sizeTimes = [1.2, 1.5]
         }
         
-        return VStack {
-            Spacer().frame(height: size.height * spacers[0])
-            HStack {
-                Spacer().frame(width: size.width * spacers[1])
-                AnyView(shape
+        return AnyView(shape
                     .strokeAndFill(fillContent: color, strokeContent: theme.borderColor.color, lineWidth: theme.borderWidth))
                 .frame(width: size.width * sizeTimes[0], height: size.height * sizeTimes[1])
-                .shadow(color: shadowColor, radius: 10, x: 5, y: 5)
+                .shadow(color: shadowColor, radius: 10, y: 5)
                 .overlay {
                     KeyLabel<Extension>(model.labelType, width: size.width, textColor: theme.suggestLabelTextColor?.color)
                         .padding(EdgeInsets(
-                            top: size.width * paddings[0],
+                            top: size.height * paddings[0],
                             leading: size.width * paddings[1],
-                            bottom: size.width * paddings[2],
+                            bottom: size.height * paddings[2],
                             trailing: size.width * paddings[3]
                         ))
                 }
                 .allowsHitTesting(false)
                 .opacity(isHidden ? 0 : 1)
-                Spacer().frame(width: size.width * spacers[2])
-            }
-            Spacer().frame(height: size.height * spacers[3])
-        }
     }
     /// その方向にViewの表示が必要な場合はサジェストのViewを、不要な場合は透明なViewを返す。
-    @ViewBuilder private func getSuggestViewIfNecessary(direction: FlickDirection) -> some View {
+    private func getSuggestViewIfNecessary(direction: FlickDirection) -> some View {
         switch suggestType {
         case .all:
             if let model = self.model.flickKeys(variableStates: variableStates)[direction] {
@@ -122,8 +109,12 @@ struct FlickSuggestView<Extension: ApplicationSpecificKeyboardViewExtension>: Vi
     var body: some View {
         VStack(spacing: tabDesign.verticalSpacing) {
             self.getSuggestViewIfNecessary(direction: .top)
+                .offset(y: size.height / 2)
+                .zIndex(1)
             HStack(spacing: tabDesign.horizontalSpacing) {
                 self.getSuggestViewIfNecessary(direction: .left)
+                    .offset(x: size.width / 2)
+                    .zIndex(1)
                 RoundedRectangle(cornerRadius: 5.0)
                     .strokeAndFill(
                         fillContent: theme.specialKeyFillColor.color,
@@ -131,9 +122,14 @@ struct FlickSuggestView<Extension: ApplicationSpecificKeyboardViewExtension>: Vi
                         lineWidth: theme.borderWidth
                     )
                     .frame(width: size.width, height: size.height)
+                    .zIndex(0)
                 self.getSuggestViewIfNecessary(direction: .right)
+                    .offset(x: -size.width / 2)
+                    .zIndex(1)
             }
             self.getSuggestViewIfNecessary(direction: .bottom)
+                .offset(y: -size.height / 2)
+                .zIndex(1)
         }
         .frame(width: size.width, height: size.height)
         .allowsHitTesting(false)
